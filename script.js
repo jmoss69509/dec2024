a768d5fb91188701b193bab46d5d743c860e0aac
jmoss69509 <100859269+jmoss69509@users.noreply.github.com>
2024-12-05 19:44:40 +0000

    firstcommit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.writeln("Script Works!");
+document.addEventListener("click", buttonclicked);
 
+function buttonclicked() {
+    document.getElementById('firstPara').style.backgroundColor="red";
+    document.getElementById('backgroundcolor').style.backgroundColor="lightblue";
+}
